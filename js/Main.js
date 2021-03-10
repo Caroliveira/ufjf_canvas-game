e15f77963077cef92a134a0f3575a4ecb4f362b2
Carolina Oliveira <10094036640@estudante.ufjf.br>
2021-03-09 21:46:21 -0300

    Adiciona lista de sprites na cena

--- js/Main.js
+++ js/Main.js
@@ -4,10 +4,10 @@ import Sprite from "./Sprite.js";
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const scene1 = new Scene(canvas);
-scene1.draw();
 
 const pc = new Sprite();
 const en1 = new Sprite({ x: 140, w: 30, color: "red" });
 
-pc.draw(ctx);
-en1.draw(ctx);
+scene1.addSprite(pc);
+scene1.addSprite(en1);
+scene1.draw();
