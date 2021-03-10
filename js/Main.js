064f25d1a808ed52da286f49272e7f4f1ec2d187
Carolina Oliveira <10094036640@estudante.ufjf.br>
2021-03-09 21:26:42 -0300

    Cria e usa uma classe de cena

--- js/Main.js
+++ js/Main.js
@@ -1,4 +1,7 @@
-console.log("hello");
+import Scene from './Scene.js';
 
 const canvas = document.querySelector("canvas");
 console.log(canvas);
+
+const scene1 = new Scene(canvas);
+scene1.draw()
---
+++ js/Scene.js
@@ -0,0 +1,12 @@
+export default class Scene {
+  // Classe responsável por desenhar elementos na tela em uma animação
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+  }
+
+  draw() {
+    this.fillStyle = "grey";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
