c5eb9bac0e21af0a705af045be5a02dbb6a6fb76
Carolina Oliveira <10094036640@estudante.ufjf.br>
2021-03-10 22:37:44 -0300

    adiciona modelo de mapa

--- js/Main.js
+++ js/Main.js
@@ -3,6 +3,7 @@ import Map from "./Map.js";
 import Mixer from "./Mixer.js";
 import Scene from "./Scene.js";
 import Sprite from "./Sprite.js";
+import modelMap1 from "../maps/map1.js";
 
 const mixer = new Mixer(10);
 const assets = new AssetManager(mixer);
@@ -14,11 +15,12 @@ assets.loadAudio("coin", "assets/coin.wav");
 assets.loadAudio("boom", "assets/boom.wav");
 
 const canvas = document.querySelector("canvas");
-canvas.width = 14*32;
-canvas.height = 10*32;
+canvas.width = 14 * 32;
+canvas.height = 10 * 32;
 const scene1 = new Scene(canvas, assets);
 
 const map1 = new Map(10, 14, 32);
+map1.loadMap(modelMap1);
 scene1.configureMap(map1);
 
 const pc = new Sprite({ vx: 10 });
--- js/Map.js
+++ js/Map.js
@@ -10,28 +10,40 @@ export default class Map {
         this.tiles[r][c] = 0;
       }
     }
-    this.scene = null
+    this.scene = null;
   }
 
   draw(ctx) {
     for (let r = 0; r < this.ROWS; r++) {
       for (let c = 0; c < this.COLUMNS; c++) {
         switch (this.tiles[r][c]) {
-            case 1:
-                ctx.fillStyle = 'grey';
-                ctx.fillRect(c*this.SIZE, r*this.SIZE, this.SIZE, this.SIZE);
-                ctx.lineWidth = 1
-                ctx.strokeStyle = "black";
-                ctx.strokeRect(c*this.SIZE, r*this.SIZE, this.SIZE, this.SIZE);
-                break;
-            default:
-                ctx.fillStyle = 'black';
-                ctx.fillRect(c*this.SIZE, r*this.SIZE, this.SIZE, this.SIZE);
-                ctx.lineWidth = 1
-                ctx.strokeStyle = "grey";
-                ctx.strokeRect(c*this.SIZE, r*this.SIZE, this.SIZE, this.SIZE);
-                break;
+          case 1:
+            ctx.fillStyle = "grey";
+            ctx.fillRect(c * this.SIZE, r * this.SIZE, this.SIZE, this.SIZE);
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = "black";
+            ctx.strokeRect(c * this.SIZE, r * this.SIZE, this.SIZE, this.SIZE);
+            break;
+          default:
+            ctx.fillStyle = "black";
+            ctx.fillRect(c * this.SIZE, r * this.SIZE, this.SIZE, this.SIZE);
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = "grey";
+            ctx.strokeRect(c * this.SIZE, r * this.SIZE, this.SIZE, this.SIZE);
+            break;
         }
+      }
+    }
+  }
+
+  loadMap(model) {
+    this.ROWS = model.length;
+    this.COLUMNS = model[0]?.length ?? 0;
+    this.tiles = [];
+    for (let r = 0; r < this.ROWS; r++) {
+      this.tiles[r] = [];
+      for (let c = 0; c < this.COLUMNS; c++) {
+        this.tiles[r][c] = model[r][c];
       }
     }
   }
